0f60d978c171839d5aadff5d32c01d8563c152d5
shivam4490 <csg9152@gmail.com>
2021-05-12 01:33:53 +0530

    adding listeners and price counts

---
+++ script.js
@@ -0,0 +1,45 @@
+const container = document.querySelector('.container');
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
+const count = document.getElementById('count');
+const total = document.getElementById('total');
+const movieSelect = document.getElementById('movie');
+
+let ticketPrice = +movieSelect.value;
+
+// Update total and count
+function updateSelectedCount() {
+   const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+   const seatsIndex = [...selectedSeats].map((seat) =>
+      [...seats].indexOf(seat)
+   );
+
+   localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+
+   const selectedSeatsCount = selectedSeats.length;
+
+   count.innerText = selectedSeatsCount;
+   total.innerText = selectedSeatsCount * ticketPrice;
+}
+
+// Movie select event
+movieSelect.addEventListener('change', (e) => {
+   ticketPrice = +e.target.value;
+   setMovieData(e.target.selectedIndex, e.target.value);
+   updateSelectedCount();
+});
+
+// Seat click event
+container.addEventListener('click', (e) => {
+   if (
+      e.target.classList.contains('seat') &&
+      !e.target.classList.contains('occupied')
+   ) {
+      e.target.classList.toggle('selected');
+
+      updateSelectedCount();
+   }
+});
+
+// Initial count and total set
+updateSelectedCount();
